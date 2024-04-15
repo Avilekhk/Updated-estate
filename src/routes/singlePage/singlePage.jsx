114c47e4bfa381b4243bd4f7b3056b15bd99d162
Avilekh Kunwar <avilekhkunwar@gmail.com>
2024-04-15 13:56:39 +1000

    Slider added

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./singlePage.scss";
+import Slider from "../../components/slider/SLider";
 
-function singlePage() {
+function SinglePage() {
   return (
-    <div>singlePage</div>
-  )
+    <div classsName="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider />
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
 
-export default singlePage
+export default SinglePage;
